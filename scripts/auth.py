c75f12bf87d51bbe651e86ca949d229c36e823c5
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-11 21:18:24 -0500

    Update auth.py

--- scripts/auth.py
+++ scripts/auth.py
@@ -10,19 +10,3 @@ def authenticate_user(username, password, user_credentials):
     user_row = user_credentials[(user_credentials['username'] == username) & (user_credentials['password'] == password)]
     return not user_row.empty
 
-# Function to handle user login
-def login():
-    st.session_state.authenticated = False
-    st.title("Login")
-
-    user_credentials = load_user_credentials('data/user_credentials.csv')
-
-    username = st.text_input("Username")
-    password = st.text_input("Password", type="password")
-
-    if st.button("Login"):
-        if authenticate_user(username, password, user_credentials):
-            st.session_state.authenticated = True
-            st.success("Login successful!")
-        else:
-            st.error("Invalid username or password")
