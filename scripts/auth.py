eeffdb659f878cf60a674ab28264227d7dfcab35
Samir Tarda <115050984+sntarda@users.noreply.github.com>
2024-07-11 21:23:48 -0500

    Update auth.py

--- scripts/auth.py
+++ scripts/auth.py
@@ -1,12 +1,13 @@
 import streamlit as st
 import pandas as pd
 
-# Function to load user credentials from a CSV file
 def load_user_credentials(file_path):
     return pd.read_csv(file_path)
 
-# Function to authenticate user
 def authenticate_user(username, password, user_credentials):
     user_row = user_credentials[(user_credentials['username'] == username) & (user_credentials['password'] == password)]
     return not user_row.empty
 
+def check_password(username, password):
+    user_credentials = load_user_credentials('data/user_credentials.csv')
+    return authenticate_user(username, password, user_credentials)
